3c42ccd8c1c41c9877c6578446e52b6189cd4d0b
RezixDev <mateuszrysownik2@gmail.com>
2024-12-28 09:02:09 +0100

    fix: base path

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: '/guitar-tab',
 };
  
 export default withNextIntl(nextConfig);
